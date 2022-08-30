5b590d0c25b17869e92c5c11f732674ccaf5ca4e
Anna Schmidt <marshall1697@gmail.com>
2022-08-30 12:18:20 -0600

    commit tree 2a4afaea1a0e4deb174e842c6c31a5006f40219f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function saturdayFun(activity){
+
+    if (activity){
+        return `This Saturday, I want to ${activity}!`
+
+    }else {
+        return 'This Saturday, I want to roller-skate!'
+    };
+}
+
+const mondayWork = function(work){
+    if (work){
+        return `This Monday, I will ${work}.`
+
+    }else {
+        return 'This Monday, I will go to the office.'
+    };
+}
+
+const wrapAdjective = function(wrap){
+    function innerFunction(adjective){
+       return adjective ? `You are ${wrap}${adjective}${wrap}!` : 'You are special!';
+       
+    }
+    return innerFunction;
+}
+
